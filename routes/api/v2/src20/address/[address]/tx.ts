8df9c01f507d41f23a8a1393a4792389f564e4ac
javiercervilla <javier@cervilla.es>
2024-09-22 15:05:53 +0200

    feat(tx):[+] Add src20/address/:address/tx endpoint

---
+++ routes/api/v2/src20/address/[address]/tx.ts
@@ -0,0 +1,39 @@
+import { Handlers } from "$fresh/server.ts";
+import { AddressTickHandlerContext } from "globals";
+import { Src20Controller } from "$lib/controller/src20Controller.ts";
+import { ResponseUtil } from "utils/responseUtil.ts";
+import { convertEmojiToTick } from "utils/util.ts";
+
+export const handler: Handlers<AddressTickHandlerContext> = {
+	async GET(req, ctx) {
+		try {
+			const { address } = ctx.params;
+			const url = new URL(req.url);
+			const params = url.searchParams;
+
+			const balanceParams = {
+				address,
+				includePagination: params.get("includePagination") === "true", // or however you determine this
+				limit: Number(params.get("limit")) || undefined,
+				page: Number(params.get("page")) || undefined,
+				amt: Number(params.get("amt")) || undefined,
+				sort: params.get("sort") || undefined,
+			};
+
+			const result = await Src20Controller.getValidSrc20Tx(balanceParams);
+
+			if (!result || Object.keys(result).length === 0) {
+				console.log("Empty result received:", result);
+				return ResponseUtil.error("No data found", 404);
+			}
+
+			return ResponseUtil.success(result);
+		} catch (error) {
+			console.error("Error in GET handler:", error);
+			return ResponseUtil.handleError(
+				error,
+				"Error processing SRC20 balance request",
+			);
+		}
+	},
+};
