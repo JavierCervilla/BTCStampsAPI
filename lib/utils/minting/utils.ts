04b8b8b3aedc4c88902e6424088ac7f1ee2b3756
javiercervilla <javier@cervilla.es>
2024-09-23 08:25:14 +0200

    feat(tx):[+] update src20/create endpoint to add fee and recipientFee.

--- lib/utils/minting/utils.ts
+++ lib/utils/minting/utils.ts
@@ -45,6 +45,14 @@ export function bin2hex(data: Uint8Array): string {
 		.join("");
 }
 
+export function arraysEqual(a: Uint8Array, b: Uint8Array): boolean {
+	if (a.length !== b.length) return false;
+	for (let i = 0; i < a.length; ++i) {
+		if (a[i] !== b[i]) return false;
+	}
+	return true;
+}
+
 export function extractOutputs(tx: btc.Transaction, address: string) {
 	const outputs = [];
 	for (const vout of tx.outs) {
