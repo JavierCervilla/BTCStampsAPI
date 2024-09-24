3a6b6826f3bad6c6951b95d356c0461b541e35d2
javiercervilla <javier@cervilla.es>
2024-09-24 11:34:34 +0200

    feat(Sentry):[+] Add error capture

--- lib/utils/responseUtil.ts
+++ lib/utils/responseUtil.ts
@@ -1,45 +1,46 @@
+import { captureException } from "utils/sentry.ts";
+
+// biome-ignore lint/complexity/noStaticOnlyClass: Better organization this way.
 export class ResponseUtil {
-  static success<T>(data: T, status: number = 200): Response {
-    return new Response(JSON.stringify(data), {
-      status,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
+	static success<T>(data: T, status = 200): Response {
+		return new Response(JSON.stringify(data), {
+			status,
+			headers: { "Content-Type": "application/json" },
+		});
+	}
 
-  static successArray<T>(data: T[], status: number = 200): Response {
-    return new Response(JSON.stringify(data), {
-      status,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
+	static successArray<T>(data: T[], status = 200): Response {
+		return new Response(JSON.stringify(data), {
+			status,
+			headers: { "Content-Type": "application/json" },
+		});
+	}
 
-  static error(message: string, status: number = 400): Response {
-    return new Response(JSON.stringify({ error: message }), {
-      status,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
+	static error(message: string, status = 400): Response {
+		return new Response(JSON.stringify({ error: message }), {
+			status,
+			headers: { "Content-Type": "application/json" },
+		});
+	}
 
-  static custom<T>(body: T, status: number): Response {
-    return new Response(JSON.stringify(body), {
-      status,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
+	static custom<T>(body: T, status: number): Response {
+		return new Response(JSON.stringify(body), {
+			status,
+			headers: { "Content-Type": "application/json" },
+		});
+	}
 
-  static notFound(message: string = "Resource not found"): Response {
-    return new Response(
-      JSON.stringify({ error: message }),
-      {
-        status: 404,
-        headers: { "Content-Type": "application/json" },
-      },
-    );
-  }
+	static notFound(message = "Resource not found"): Response {
+		return new Response(JSON.stringify({ error: message }), {
+			status: 404,
+			headers: { "Content-Type": "application/json" },
+		});
+	}
 
-  static handleError(error: unknown, defaultMessage: string): Response {
-    console.error(error);
-    const message = error instanceof Error ? error.message : defaultMessage;
-    return this.error(message, 500);
-  }
+	static handleError(error: unknown, defaultMessage: string): Response {
+		console.error(error);
+		captureException(error);
+		const message = error instanceof Error ? error.message : defaultMessage;
+		return ResponseUtil.error(message, 500);
+	}
 }
